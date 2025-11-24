44f71182b99d723831f187d5d2e5ff6c821cd518
yeekrim <kr031106@naver.com>
2025-11-24 21:18:20 +0900

    FIX: 모델 위치 변경 (깃->Google Drive)

--- ASL_translation/test_model.py
+++ ASL_translation/test_model.py
@@ -1,7 +1,7 @@
 # test_model.py
 from transformers import T5ForConditionalGeneration, T5TokenizerFast
 
-MODEL_PATH = "ASL_translation/my_finetuned_t5_model"
+MODEL_PATH = MODEL_PATH = "/Users/kyungrim/Library/CloudStorage/GoogleDrive-20221999@edu.hanbat.ac.kr/내 드라이브/2025캡스톤프로젝트/my_finetuned_t5_model"
 
 try:
     tokenizer = T5TokenizerFast.from_pretrained(MODEL_PATH)
